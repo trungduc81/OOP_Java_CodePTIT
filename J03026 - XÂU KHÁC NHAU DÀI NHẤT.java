277d2e2a92fef78d895a6e78037dfd4e7e5268c9
Trần Đức Trung <ductrung081105@gmail.com>
2025-09-21 18:02:46 +0700

    Create J03026 - XÂU KHÁC NHAU DÀI NHẤT.java

---
+++ J03026 - XÂU KHÁC NHAU DÀI NHẤT.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class NewClass {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            String s1 = sc.next(), s2 = sc.next();
+            if(s1.equals(s2)) {
+                System.out.println("-1");
+            } else {
+                System.out.println(Math.max(s1.length(), s2.length()));
+            }
+        }
+    }
+}
