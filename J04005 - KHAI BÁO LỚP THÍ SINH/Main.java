767a7d8ae3cca6cfcfbf1aa87d8d1a3050aef22a
Trần Đức Trung <ductrung081105@gmail.com>
2025-09-29 12:19:22 +0700

    Create Main.java

---
+++ J04005 - KHAI BÁO LỚP THÍ SINH/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ; 
+        String ten = sc.nextLine() ; 
+        String ngaysinh = sc.nextLine() ; 
+        double d1 = sc.nextDouble() , d2 = sc.nextDouble() , d3 = sc.nextDouble() ; 
+        ThiSinh a = new ThiSinh(ten,ngaysinh,d1,d2,d3) ; 
+        System.out.println(a);
+    }
+}
