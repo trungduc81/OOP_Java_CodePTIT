f948d55b2462d25c210b0c463083db3150fd563d
Trần Đức Trung <ductrung081105@gmail.com>
2025-09-29 12:20:57 +0700

    Create Main.java

---
+++ J04003 - PHÂN SỐ/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong() , b = sc.nextLong() ; 
+        PhanSo ps = new PhanSo(a,b) ; 
+        ps.RutGon();
+        System.out.println(ps);
+    }
+}
