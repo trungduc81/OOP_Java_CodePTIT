5342d37d7f534fcdf3725b89ef720303fd9f622e
Trần Đức Trung <ductrung081105@gmail.com>
2025-10-06 17:24:00 +0700

    Create Main.java

---
+++ J07045 - LOẠI PHÒNG/Main.java
@@ -0,0 +1,17 @@
+import java.io.* ; 
+import java.util.* ; 
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ArrayList<LoaiPhong> ds = new ArrayList<>();
+        Scanner in = new Scanner(new File("PHONG.in"));
+        int n = Integer.parseInt(in.nextLine());
+        while(n-->0){
+            ds.add(new LoaiPhong(in.nextLine()));
+        }
+        Collections.sort(ds);
+        for(LoaiPhong tmp : ds){
+            System.out.println(tmp);
+        }
+    }
+}
