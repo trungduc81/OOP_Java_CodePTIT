7eaab4bea88c11a83cf8d181090502b84b1ffc6d
Trần Đức Trung <ductrung081105@gmail.com>
2025-11-30 12:22:27 +0700

    Create J05047 - BẢNG KÊ NHẬP KHO SẮP XẾP THEO CHIẾT KHẤU.java

---
+++ J05047 - BẢNG KÊ NHẬP KHO SẮP XẾP THEO CHIẾT KHẤU.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class DonHang implements Comparable<DonHang> {
+    private String tenHang, maHang;
+    private int thanhTien, chietKhau;
+    private static Map<String, Integer> cnt = new HashMap<>();
+
+    public DonHang(String tenHang, int soLuong, int donGia) {
+        this.tenHang = tenHang;
+
+        String[] arr = tenHang.trim().split("\\s+");
+        String tmp = "" + Character.toUpperCase(arr[0].charAt(0)) + Character.toUpperCase(arr[1].charAt(0));
+        int id = cnt.getOrDefault(tmp, 0) + 1;
+        cnt.put(tmp, id);
+        this.maHang = tmp + String.format("%02d", id);
+
+        if (soLuong > 10) this.chietKhau = 5 * donGia * soLuong / 100;
+        else if (soLuong >= 8) this.chietKhau = 2 * donGia * soLuong / 100;
+        else if (soLuong >= 5) this.chietKhau = donGia * soLuong / 100;
+        else this.chietKhau = 0;
+
+        this.thanhTien = donGia * soLuong - this.chietKhau;
+    }
+
+    @Override
+    public String toString() {
+        return maHang + " " + tenHang + " " + chietKhau + " " + thanhTien;
+    }
+
+    @Override
+    public int compareTo(DonHang o) {
+       return o.chietKhau - this.chietKhau ; 
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        ArrayList<DonHang> ds = new ArrayList<>() ; 
+        sc.nextLine();
+        while (n-- > 0) {
+            DonHang dh = new DonHang(sc.nextLine(), sc.nextInt(), sc.nextInt());
+            sc.nextLine();
+            ds.add(dh) ; 
+        }
+        Collections.sort(ds) ; 
+        for(DonHang i : ds){
+            System.out.println(i);
+        }
+    }
+}
